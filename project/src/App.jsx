07fea6a081c3fb0eee716c4d0f637af9ae9b9f4e
LeoSntos <leobrognoli38@gmail.com>
2024-04-04 17:57:56 -0300

    exercício 4 (adição de props no CardTrilha)

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,8 +1,28 @@
-import CardTrilha from ".components/CardTrilha"
+import CardTrilha from "./components/CardTrilha"
 
 function App() {
-  return(
+
+  const listaDeTrilhas = [
+    {
+      nomeTrilha: "Trilha da Costa da Lagoa",
+      cidade: "Florianópolis",
+      estado: "SC",
+      duracao:  120,
+      trajeto: 4,
+      dificuldade:  "iniciante!",
+      tipoDeTrilha: "caminhada / trekking",
+      nomeDoCriador: "Leonardo Brognoli",
+      urlImgTrilha: "https://images.pexels.com/photos/917510/pexels-photo-917510.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    }
+  ]
+
+  return (
     <>
+    {
+      listaDeTrilhas.map((trilha, index) => (
+      <CardTrilha dadosTrilha={trilha} key={index}/>
+      ))
+    }
     </>
   )
 }
--- project/src/components/CardTrilha/index.jsx
+++ project/src/components/CardTrilha/index.jsx
@@ -1,8 +1,33 @@
-function CardTrilha() {
-    return(
+import * as PropTypes from "prop-types"
+
+function CardTrilha({ dadosTrilha }) {
+
+    return (
         <>
+            <h1>{dadosTrilha.nomeTrilha}</h1>
+            <span>{dadosTrilha.cidade} / {dadosTrilha.estado}</span>
+            <h1>Duraçao: {dadosTrilha.duracao} Min</h1>
+            <h1>Trajeto: {dadosTrilha.trajeto} Km</h1>
+            <h1>{dadosTrilha.dificuldade}</h1>
+            <h1>{dadosTrilha.tipoDeTrilha}</h1>
+            <h1>Por: {dadosTrilha.nomeDoCriador}</h1>
+            <img width={200} src={dadosTrilha.urlImgTrilha} alt="imagem trilha"></img>
         </>
     )
+}
+
+CardTrilha.propTypes = {
+    dadosTrilha: PropTypes.exact({
+        nomeTrilha: PropTypes.string.isRequired,
+        cidade: PropTypes.string.isRequired,
+        estado: PropTypes.string.isRequired,
+        duracao: PropTypes.number.isRequired,
+        trajeto: PropTypes.number.isRequired,
+        dificuldade: PropTypes.string.isRequired,
+        tipoDeTrilha: PropTypes.oneOf(['caminhada / trekking', 'ciclismo']),
+        nomeDoCriador: PropTypes.string.isRequired,
+        urlImgTrilha: PropTypes.string.isRequired,
+    })
 }
 
 export default CardTrilha;
