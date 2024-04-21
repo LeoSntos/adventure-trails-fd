a8d421fc96bd94e04b8d59eb67dee673faebdfb5
LeoSntos <leobrognoli38@gmail.com>
2024-04-21 17:25:39 -0300

    exercício 4 (Criando página inicial)

--- project/src/components/HeaderTrilha/index.jsx
+++ project/src/components/HeaderTrilha/index.jsx
@@ -1,4 +1,5 @@
 import "./style.css"
+import {Link} from "react-router-dom"
 
 function HeaderTrilha() {
 
@@ -9,8 +10,8 @@ function HeaderTrilha() {
                 <h1>Adventure Trails FD</h1>
             </div>
 
-                <a href="#" id="primeira-ancora">Explorar Trilhas</a>
-                <a href="#" id="segunda-ancora">Cadastrar Trilhas</a>
+                <Link to="/lista-trilha" id="primeira-ancora">Explorar Trilhas</Link>
+                <Link to="/cadastro-trilha" id="segunda-ancora">Cadastrar Trilhas</Link>
         </div>
     )
 }
