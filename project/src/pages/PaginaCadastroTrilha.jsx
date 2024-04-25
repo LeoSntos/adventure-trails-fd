105afbcf73749e54b8cf98a1b791ca5d552ba01d
LeoSntos <leobrognoli38@gmail.com>
2024-04-25 15:17:49 -0300

    Exercício 2 (Faça o registro dos campos do formulário)

--- project/src/pages/PaginaCadastroTrilha.jsx
+++ project/src/pages/PaginaCadastroTrilha.jsx
@@ -1,20 +1,22 @@
 import { TextField, MenuItem, } from '@mui/material'
 import styles from "./PaginaCadastroTrilha.module.css"
-import { node } from 'prop-types';
+import { useForm } from "react-hook-form"
 
 function PaginaCadastroTrilha() {
+    const { register, handleSubmit, formState: { errors } } = useForm()
+
     const Dificuldade = [
         {
             value: 'Facil',
             label: 'Facil',
         },
         {
-            value: 'Dificil',
-            label: 'Dificil',
+            value: 'Intermediario',
+            label: 'Intermediario',
         },
         {
-            value: 'Muito-dificil',
-            label: 'Muito dificil',
+            value: 'Dificil',
+            label: 'Dificil',
         },
     ];
 
@@ -41,25 +43,64 @@ function PaginaCadastroTrilha() {
 
             <form>
 
-                <TextField type='text' label="Nome da trilha" margin='normal' sx={ {width: 572, height: 60}}></TextField>
+                <TextField placeholder='' type='text' label="Nome da trilha" margin='normal' sx={{ width: 572, height: 60 }} {...register("nomeTrilha", {
+                    required: "Insira o nome da trilha",
+                    maxLength: {
+                        value: 100,
+                        message: "limite de 100 caracteres"
+                    }
+                })}></TextField>
 
                 <div>
-                    <TextField type='number' label="Duraçao estimada (min)" margin='normal' sx={{width: 286}}></TextField>
-                    <TextField type='number' label="Trajeto (Km)" margin='normal' sx={{width: 286}} ></TextField>
+                    <TextField type='number' label="Duraçao estimada (min)" margin='normal' sx={{ width: 286 }} {...register("duracao", {
+                        required: "É precios de um numero",
+                        maxLength: {
+                            value: 4,
+                            message: "Apenas 4 caracteres"
+                        }
+                    })}></TextField>
+
+                    <TextField type='number' label="Trajeto (Km)" margin='normal' sx={{ width: 286 }} {...register("trajeto", {
+                        required: "É precios de um numero",
+                        maxLength: {
+                            value: 5,
+                            message: "Apenas 5 caracteres"
+                        }
+                    })}></TextField>
+                </div>
+
+                <div>
+                    <TextField type='text' label="Cidade" margin='normal' sx={{ width: 286 }} {...register("cidade", {
+                        required: "Insira o nome da cidade",
+                        maxLength: {
+                            value: 60,
+                            message: "limite de 60 caracteres"
+                        }
+                    })}></TextField>
+                    <TextField type='text' label="Estado" margin='normal' sx={{ width: 286 }}  {...register("estado", {
+                        required: "Insira o nome do estado",
+                        maxLength: {
+                            value: 2,
+                            message: "limite de 2 caracteres"
+                        }
+                    })}></TextField>
                 </div>
                 <div>
-                    <TextField type='text' label="Cidade" margin='normal' sx={{width: 286}} ></TextField>
-                    <TextField type='text' label="Estado" margin='normal' sx={{width: 286}} ></TextField>
-                </div>
-                <div>
-                    <TextField type='text' label="Nome completo do usuario" margin='normal' sx={{width: 286}}></TextField>
+                    <TextField type='text' label="Nome completo do usuario" margin='normal' sx={{ width: 286 }} {...register("nomeUsuario", {
+                        maxLength: {
+                            value: 60,
+                            message: "limite de 60 caracteres"
+                        }
+                    })}></TextField>
 
                     <TextField
-                        id="outlined-select-currency"
                         select
                         label="Dificuldade"
                         margin='normal'
-                        sx={{width: 286}}
+                        sx={{ width: 286 }}
+                        {...register("dificuldade", {
+                            required: "Você deve selecionar uma dificuladade",
+                        })}
                     >
                         {Dificuldade.map((option) => (
                             <MenuItem key={option.value} value={option.value}>
@@ -71,11 +112,13 @@ function PaginaCadastroTrilha() {
 
 
                 <TextField
-                    id="outlined-select-currency"
                     select
                     label="Tipo de trilha"
                     margin='normal'
-                    sx={ {width: 572, height: 60}}
+                    sx={{ width: 572, height: 60 }}
+                    {...register("tipoTrilha", {
+                        required: "Você deve selecionar uma dificuladade",
+                    })}
                 >
                     {TipoTrilha.map((option) => (
                         <MenuItem key={option.value} value={option.value}>
@@ -85,7 +128,12 @@ function PaginaCadastroTrilha() {
                 </TextField>
 
                 <div>
-                    <TextField type='text' label="Url da trilha" margin='normal' sx={ {width: 572, height: 60}}></TextField>
+                    <TextField type='text' label="Url da trilha" margin='normal' sx={{ width: 572, height: 60 }} {...register("urlTrilha", {
+                        maxLength: {
+                            value: 300,
+                            message: "A url deve conter 300 caracteres"
+                        }
+                    })}></TextField>
                 </div>
 
             </form>
